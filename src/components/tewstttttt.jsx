5b14806504bec89359f8a94b0c33c85879730d12
AnshikX <athakur@argusoft.com>
2025-06-04 12:35:51 +0530

    sd

--- src/components/tewstttttt.jsx
+++ src/components/tewstttttt.jsx
@@ -3,7 +3,15 @@ import { Alert } from "react-bootstrap";
 const Tewstttttt = () => {
   return (
     <div hidden={false} id={"sa"} onClick={"ds"} onPaste={undefined}>
-      <Alert show={"tru"} />
+      Hello World
+      <div className={"row"} />
+      <div className={"row"} />
+      <Alert variant={"sa"}>Hello World</Alert>
+      <div className={"row"}>
+        <div className={"col"} />
+        <div className={"col"} />
+        <div className={"col"} />
+      </div>
     </div>
   );
 };
