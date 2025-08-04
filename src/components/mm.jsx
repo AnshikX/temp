1f24b5f2229199aef1db785932199d2212a3b56e
AnshikX <athakur@argusoft.com>
2025-08-04 12:11:11 +0530

    s

--- src/components/mm.jsx
+++ src/components/mm.jsx
@@ -1,5 +1,24 @@
 import { React } from "react";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import D from "/src/components/d.jsx";
 const Mm = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>Hello World</Col>
+          <Col>Hello World</Col>
+        </Row>
+        <Row>
+          <Col>Hello World</Col>
+          <Col>Hello World</Col>
+        </Row>
+      </Container>
+      <D />
+      <div className={"row"} />
+    </div>
+  );
 };
 export default Mm;
