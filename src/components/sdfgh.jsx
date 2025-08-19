3bc80c17e7e5b915508b73dc88cc548d1fdc6f86
AnshikX <athakur@argusoft.com>
2025-08-19 12:21:42 +0530

    d

--- src/components/sdfgh.jsx
+++ src/components/sdfgh.jsx
@@ -1,5 +1,10 @@
 import { React } from "react";
 const Sdfgh = () => {
-  return <div className={""}>Hello world</div>;
+  return (
+    <div className={""}>
+      Hello world
+      <div className={"row"} />
+    </div>
+  );
 };
 export default Sdfgh;
